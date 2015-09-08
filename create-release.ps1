004806e0e4bc12bb6da3ae14e6723d671bfc0b2f
Feodor Fitsner <feodor@appveyor.com>
2015-09-08 12:45:29 -0700

    Added create-release.ps1 [skip ci]

---
+++ create-release.ps1
@@ -0,0 +1,14 @@
+$headers = @{
+  "Authorization" = "Bearer $env:gh_token"
+  "Content-type" = "application/json"
+}
+
+$body = ConvertTo-Json @{
+  "tag_name" = "$env:appveyor_build_version"
+  "target_commitish" = "$env:appveyor_repo_commit"
+  "name" = "$env:appveyor_build_version"
+  "draft" = $false
+  "prerelease" = $false
+}
+
+Invoke-RestMethod -Uri 'https://api.github.com/repos/FeodorFitsner/WebJob-Appevyor/releases' -Headers $headers -Method Post -Body $body
